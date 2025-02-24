d59f7c94beb1fd0c50af575a578146f951d2d889
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2025-02-24 19:17:51 +0100

    update EditRelease.vue

--- resources/js/Pages/EditRelease.vue
+++ resources/js/Pages/EditRelease.vue
@@ -44,7 +44,8 @@ const form = useForm({
 
     release_type_id: props.release.release_type_id || null,
     release_format_ids: props.release.release_formats?.map(format => format.id) || [],
-
+    reference_member_id: props.release.release_members?.findIndex(member => member.is_reference) ?? 0,
+    
     tracks: props.release.release_tracks?.length > 0 
         ? props.release.release_tracks.map(track => ({
             id: track.id,
@@ -197,72 +198,72 @@ const submit = () => {
                 <div class="p-8">
                     <div class="space-y-6 border-gray-700 pb-6">
                         <h3 class="text-xl font-bold text-gray-100 flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-4 size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                            </svg>
-                            Informations sur le(s) membre(s)</h3>
-                            <InputLabel value="" class="text-sm font-medium" />
-                                <table class="min-w-full rounded-md overflow-hidden border-gray-700 !mt-1">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col" class="w-16 px-3 py-2.5 text-left text-sm font-semibold bg-gray-700 text-gray-100 whitespace-nowrap">Prénom</th>
-                                            <th scope="col" class="w-full px-3 py-2.5 text-left text-sm font-semibold bg-gray-700 text-gray-100 whitespace-nowrap">Nom</th>
-                                            <th scope="col" class="w-16 px-3 py-2.5 text-sm font-semibold bg-gray-700 text-gray-100">
-                                                <button 
-                                                        type="button" 
-                                                        @click="addNewMember"
-                                                        class="w-9 h-9 bg-indigo-600 text-white text-sm rounded-md hover:bg-indigo-700 transition-colors flex items-center justify-center"
-                                                    >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                                    </svg>
-                                                </button>
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="divide-y divide-gray-700">
-                                        <tr v-for="(member, index) in form.members" :key="member.id || 'new'" class="bg-gray-700/50">
-                                            <td class="px-1.5 py-2">
-                                                <TextInput
-                                                    type="text"
-                                                    v-model="member.firstname"
-                                                    class="transition duration-150 ease-in-out"
-                                                    placeholder="Titre"
-                                                />
-                                            </td>
-                                            <td class="px-1.5 py-2">
-                                                <TextInput
-                                                    type="text"
-                                                    v-model="member.lastname"
-                                                    class="w-full transition duration-150 ease-in-out"
-                                                    placeholder="Titre"
-                                                />
-                                            </td>
-                                            <td class="whitespace-nowrap px-3 py-2">
-                                                <button
-                                                    v-if="form.members.length > 1 && index === form.members.length - 1"
-                                                    type="button" 
-                                                    @click="deleteMember(index)"
-                                                    class="w-9 h-9 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition-colors flex items-center justify-center"
-                                                >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                    </svg>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <InputLabel value="Membre de référence" class="text-sm font-medium mb-1" />
-                                <select
-                                    v-model="form.reference_member_id"
-                                    @change="updateReferenceMember"
-                                    class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                >
-                                    <option v-for="(member, index) in form.members" :key="member.id || 'new'" :value="index">
-                                        {{ member.firstname }} {{ member.lastname }}
-                                    </option>
-                                </select>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-4 size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                        </svg>
+                        Informations sur le(s) membre(s)</h3>
+                        <InputLabel value="" class="text-sm font-medium" />
+                        <table class="min-w-full rounded-md overflow-hidden border-gray-700 !mt-1">
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="w-16 px-3 py-2.5 text-left text-sm font-semibold bg-gray-700 text-gray-100 whitespace-nowrap">Prénom</th>
+                                    <th scope="col" class="w-full px-3 py-2.5 text-left text-sm font-semibold bg-gray-700 text-gray-100 whitespace-nowrap">Nom</th>
+                                    <th scope="col" class="w-16 px-3 py-2.5 text-sm font-semibold bg-gray-700 text-gray-100">
+                                        <button 
+                                                type="button" 
+                                                @click="addNewMember"
+                                                class="w-9 h-9 bg-indigo-600 text-white text-sm rounded-md hover:bg-indigo-700 transition-colors flex items-center justify-center"
+                                            >
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                            </svg>
+                                        </button>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-700">
+                                <tr v-for="(member, index) in form.members" :key="member.id || 'new'" class="bg-gray-700/50">
+                                    <td class="px-1.5 py-2">
+                                        <TextInput
+                                            type="text"
+                                            v-model="member.firstname"
+                                            class="transition duration-150 ease-in-out"
+                                            placeholder="Titre"
+                                        />
+                                    </td>
+                                    <td class="px-1.5 py-2">
+                                        <TextInput
+                                            type="text"
+                                            v-model="member.lastname"
+                                            class="w-full transition duration-150 ease-in-out"
+                                            placeholder="Titre"
+                                        />
+                                    </td>
+                                    <td class="whitespace-nowrap px-3 py-2">
+                                        <button
+                                            v-if="form.members.length > 1 && index === form.members.length - 1"
+                                            type="button" 
+                                            @click="deleteMember(index)"
+                                            class="w-9 h-9 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition-colors flex items-center justify-center"
+                                        >
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <InputLabel value="Membre de référence" class="text-sm font-medium mb-1" />
+                        <select
+                            v-model="form.reference_member_id"
+                            @change="updateReferenceMember"
+                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
+                        >
+                            <option v-for="(member, index) in form.members" :key="member.id || 'new'" :value="index">
+                                {{ member.firstname }} {{ member.lastname }}
+                            </option>
+                        </select>
                     </div>
                 </div>
             </div>
