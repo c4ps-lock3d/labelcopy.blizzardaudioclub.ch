2b97c68c70e64aa1f426cd7d8261d2ea0f51ed05
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2025-04-02 21:52:27 +0200

    update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- DaisyUI -->
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
