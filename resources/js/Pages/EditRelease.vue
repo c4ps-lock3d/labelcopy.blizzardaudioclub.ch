fd24cad712b275352fde23f2d630ff5a6dbd1ba0
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2025-04-06 21:11:49 +0200

    update ReleaseController.php and EditRelease.vue

--- resources/js/Pages/EditRelease.vue
+++ resources/js/Pages/EditRelease.vue
@@ -58,7 +58,6 @@ const form = useForm({
     artistName: props.release.artistName,
     facebook: props.release.facebook,
     instagram: props.release.instagram,
-    tiktok: props.release.tiktok,
     youtube: props.release.youtube,
     bandcamp: props.release.bandcamp,
     applemusic: props.release.applemusic,
@@ -472,9 +471,9 @@ const submit = () => {
                                 <TextArea
                                     id="artistBiography"
                                     type="text"
-                                    class="mt-1 pb-[0.87rem] block w-full transition duration-150 ease-in-out"
+                                    class="mt-1 block w-full transition duration-150 ease-in-out"
                                     v-model="form.artistBiography"
-                                    rows="15"
+                                    rows="13"
                                     required
                                     autocomplete="artistBiography"
                                     :disabled="isDisabled"
@@ -534,22 +533,6 @@ const submit = () => {
                                         </div>
                                     </div>
                                     <InputError class="mt-2" :message="form.errors.instagram" />
-                                    <div class="flex items-center gap-4">
-                                        <div class="relative flex items-center w-full">
-                                            <svg class="absolute left-3 size-5 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                <path fill="currentColor" d="M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 15.54 3h-3.09v12.4a2.592 2.592 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48"/>
-                                            </svg>
-                                            <TextInput
-                                                id="tiktok"
-                                                type="text"
-                                                class="pl-10 w-full transition duration-150 ease-in-out"
-                                                v-model="form.tiktok"
-                                                placeholder="TikTok"
-                                                :disabled="isDisabled"
-                                            />
-                                        </div>
-                                    </div>
-                                    <InputError class="mt-2" :message="form.errors.tiktok" />
                                     <div class="flex items-center gap-4">
                                         <div class="relative flex items-center w-full">
                                             <svg class="absolute left-3 size-5 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
